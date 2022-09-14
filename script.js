d525c8bfb92621294e0dff94c376bf98d9e4f184
Gilson-eng <gm_vieira@hotmail.com>
2022-09-14 19:29:50 -0300

    Atualização do site

--- script.js
+++ script.js
@@ -1,13 +1,35 @@
-function Enviar() {
-    let name = document.getElementById("nome").value;
-    if (name === '') {
-        alert("O campo nome não pode estar em branco!");
+function contatos() {
+    const element = document.getElementById("formcontatos");
+    if (element.style.display == "flex")
+        element.style.display = "none";
+    else {
+        element.style.display = "flex";
     }
-    else{
-        
-    }
-
 }
+
+document.forms.formulario.onsubmit = e => {
+    e.preventDefault();
+
+    const form = e.target;
+    const formData = new FormData(form);
+
+    const formularioDados = {
+        nome:formData.get("nome"),
+        cpf:formData.get("cpf"),
+        telefone:formData.get("telefone"),
+        email:formData.get("email"),
+        estado:formData.get("select"),
+        genero:formData.get("genero"),
+        mensagem:formData.get("mensagem")
+    };
+
+    console.log(formularioDados);
+    console.log(JSON.stringify(formularioDados));
+    
+}
+
+
+
 
 
 
